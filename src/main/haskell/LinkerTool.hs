18f1426fbe178b652d93ace3ceaf1e0eca7ce504
Joao Nicola <joaoraf@gmail.com>
2019-10-09 13:58:52 -0300

    Suporte a parse em diferentes contextos no linkertool mediante intercalamento de URN, Texto, e linha final com ###LEXML-END###, usando a opcao --contexto=INLINE

--- src/main/haskell/LinkerTool.hs
+++ src/main/haskell/LinkerTool.hs
@@ -20,6 +20,7 @@ import qualified Data.ByteString as BIO
 import Data.ByteString.UTF8 (toString, fromString)
 import System.IO
 import LexML.MD5
+import GHC.IO.Handle.FD (stdin,stdout)
 
 data Analise = Analise {
       frase :: String
@@ -53,19 +54,57 @@ analise = Analise {
 --          , XML  &= help "Gera XML decorado com as URNs reconhecidas"
         ] 
     , enderecoResolver = "http://homologa.lexml.gov.br/urn/URNLEXML" &= opt "http://homologa.lexml.gov.br/urn/URNLEXML" &= help "Endereço do LexML Resolver, a string URNLEXML será substituída pela URN. Usado quando com o tipo de saída HTML"
-    , contexto = "federal" &= typ "URN" &= opt "federal" &= help "URN Lex ML de contexto. Podem ser usados os apelidos 'federal' e 'senado' para o contexto de leis federais e resoluções do senado, respectivamente" 
+    , contexto = "federal" &= typ "URN" &= opt "federal" &= help "URN Lex ML de contexto. Podem ser usados os apelidos 'federal' e 'senado' para o contexto de leis federais e resoluções do senado, respectivamente, ou INLINE para ser lido alternadamente às linhas de entrada" 
     , logaRegras = False &= help "Loga a execução das regras"
     , logaTokens = False &= help "Loga a saída da análise léxica"
     , calculaMD5 = False &= help "Calcula o MD5 da entrada"
     , debug = False &= help "Mostra mensagens de debug"
   }
 
+endMarker :: BIO.ByteString
+endMarker = fromString "###LEXML-END###"
+
+readUntilEndMarker handle = readUntilEndMarker' []
+  where
+    readUntilEndMarker' l = do
+      eof <- hIsEOF handle
+      if eof then return $ reverse l 
+      else do
+        line <- BIO.hGetLine handle
+        if line == endMarker then return $ reverse l 
+          else readUntilEndMarker' (line : l)          
+
+readInput :: LinkerOptions -> Bool -> Maybe Handle -> (LinkerOptions -> String -> IO ()) -> IO ()
+readInput lo ctxInline Nothing proc = readInput lo ctxInline (Just stdin) proc
+readInput lo False (Just handle) proc = do
+  cts <- BIO.hGetContents handle
+  proc lo (toString cts)
+readInput lo True (Just handle) proc = do
+  eof <- hIsEOF handle
+  if eof then return () else do 
+    context <- hGetLine handle
+    if context == "" then return () else do 
+      ctx <- parseContexto context
+      let lo' = lo { loContext = ctx }
+      lines <- readUntilEndMarker handle
+      proc lo' $ toString $ BIO.intercalate (fromString "\n") lines
+      readInput lo True (Just handle) proc
+
+      
+parseContexto c@('u':'r':'n':':':_) = do
+  mu <- parseURNLexML c
+  case mu of
+    Nothing -> return $ RC_Nome "federal"
+    Just u  -> return $ RC_URNLexML u
+parseContexto c = return $ RC_Nome c
 
 main = do
   args <- cmdArgs analise
   let logLevel = if debug args then DEBUG else ERROR
   updateGlobalLogger rootLoggerName (setLevel logLevel)
+  let ctxInline = contexto args == "INLINE" 
   contexto <- case contexto args of
+                "INLINE" -> return $ RC_Nome "federal"
                 "" -> return $ RC_Nome "federal"
                 c@('u':'r':'n':':':_) -> do
                     mu <- parseURNLexML c
@@ -87,30 +126,34 @@ main = do
                 HXML -> IT_TAGGED
           , loDebugRegras = logaRegras args
           , loDebugTokens = logaTokens args
-        }
-  inputBS <- case frase args of
-              "" -> case entrada args of
-                    ""  -> BIO.getContents
-                    "-" -> BIO.getContents
-                    fname -> BIO.readFile fname
-              f -> return  $ fromString f              
-  when (calculaMD5 args) $ hPutStrLn stderr $ "MD5: " ++ md5sum inputBS
-  let input = toString inputBS
-  res <- runExceptT $ linker lo input
-  hSetEncoding stdout utf8 
-  case res of
-    Left err -> return ()
-    Right out -> do
-      let content = resContent out
-      case saida args of
-        --"" -> IOUTF.putStr content
-        "" -> putStr content
-        --"-" -> IOUTF.putStr content
-        "-" -> putStr content
-        --fname -> IOUTF.writeFile fname content
-        fname -> withFile fname WriteMode $ \h -> do
-           hSetEncoding h utf8
-           hPutStr h content
-  
-
-
+        }        
+  let inputProc :: (LinkerOptions -> String -> IO ()) -> IO ()
+      inputProc = case frase args of
+        ""  -> (case entrada args of
+          "" -> readInput lo ctxInline Nothing 
+          "-" -> readInput lo ctxInline Nothing
+          fname -> \proc -> (
+            do handle <- openFile fname ReadMode
+               readInput lo ctxInline (Just handle) proc
+               hClose handle 
+            )
+          )
+        txt -> \proc -> proc lo txt
+       
+  outHandle <- case saida args of
+        "" -> return $ stdout
+        "-" -> return $ stdout
+        fname -> openFile fname WriteMode
+  hSetEncoding outHandle utf8
+  inputProc $ \lo input -> do
+    res <- runExceptT $ linker lo input
+    case res of
+      Left err -> return ()
+      Right out -> do
+        let content = resContent out
+        hPutStr outHandle content
+        when ctxInline $ do
+          hPutStrLn outHandle "" 
+          hPutStrLn outHandle "###LEXML-END###" 
+  hClose outHandle 
+  return ()
